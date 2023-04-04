4a92738c73d74fdecd18e9faf1c19d92d6032562
Daniela Schneiderová <kozlovadaniela8@gmail.com>
2023-04-04 13:50:19 +0200

    přidání pseudoelement cross/circle a vyzkoušení v první řadě.

---
+++ hra.js
@@ -0,0 +1,13 @@
+console.log('funguju');
+/*
+const cells = document.querySelectorAll('.cell');
+
+cells.forEach((cell) => {
+  cell.addEventListener('click', () => {
+    if (cell.innerHTML === '') {
+      cell.innerHTML =
+        '<svg width="100" height="100"><circle cx="50" cy="50" r="40" stroke="black" stroke-width="3" fill="transparent"/></svg>';
+    }
+  });
+});*/
+let currentPlayer = 'circle'
