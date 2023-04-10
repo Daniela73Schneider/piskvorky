99dce0b41b74db94563b092ddeaed3b455439c7c
Daniela Schneiderová <kozlovadaniela8@gmail.com>
2023-04-10 15:05:02 +0200

    změna uživatele, přidávání circle a cross dle hráče

--- hra.js
+++ hra.js
@@ -1,35 +1,51 @@
 console.log('funguju');
-//S každým kliknutím změň hodnotu proměnné currentPlayer na opačnou. Z circle na cross a naopak.
 
 let currentPlayer = 'circle';
-const currentPlayerSymbol = document.getElementById('currentPlayer');
-function nextPlayer() {
+
+const playerCross = `
+  <svg class = 'cross' viewBox="0 0 20 20" width="20" height="20" style="display: inline-block; border-radius: 50%; fill: transparent ;">
+    <line x1="0" y1="0" x2="20" y2="20" stroke="white" stroke-width="2"/>
+    <line x1="0" y1="20" x2="20" y2="0" stroke="white" stroke-width="2"/>
+  </svg>
+`;
+
+const buttonElm = document.querySelector('button');
+const button2 = document.querySelector('button:nth-child(2)');
+const button3 = document.querySelector('button:nth-child(3)');
+const button4 = document.querySelector('button:nth-child(4)');
+const button5 = document.querySelector('button:nth-child(5)');
+const button6 = document.querySelector('button:nth-child(6)');
+const button7 = document.querySelector('button:nth-child(7)');
+const button8 = document.querySelector('button:nth-child(8)');
+const button9 = document.querySelector('button:nth-child(9)');
+const button10 = document.querySelector('button:nth-child(10)');
+
+const addMark = (event) => {
   if (currentPlayer === 'circle') {
+    event.target.classList.add('fieldCircle');
+    event.target.disabled = true;
+    const circleElm = document.querySelector('span.dot');
+    console.log(circleElm);
+    const playerElm = document.querySelector('span.dot');
+    playerElm.innerHTML = playerCross;
     currentPlayer = 'cross';
   } else {
+    event.target.classList.add('fieldCross');
+    event.target.disabled = true;
+    const crossElm = document.querySelector('.cross');
+    console.log(crossElm);
+    crossElm.remove();
     currentPlayer = 'circle';
   }
-  updateCurrentPlayerSymbol();
-}
-function updateCurrentPlayerSymbol() {
-  if (currentPlayer === 'circle') {
-    currentPlayerSymbol.innerHTML = '○';
-  } else {
-    currentPlayerSymbol.innerHTML = 'x';
-  }
-}
-const buttons = document.querySelectorAll('button');
-buttons.forEach((button) => {
-  button.addEventListener('click', (event) => {
-    event.target.classList.add('circle');
-  });
-});
-/* s child mi to nefunguje, dám jeden circle a konec! zjistit jak to jinak udělat.
-const buttonElm = document.querySelectorAll('button:nth-child(2n)');
-buttonElm.addEventListener('click', (event) =>
-  event.target.classList.add('circle'),
-);
+};
 
-// , else(
-// event.target.classList.add('cross');)
-//});*/
+buttonElm.addEventListener('click', addMark);
+button2.addEventListener('click', addMark);
+button3.addEventListener('click', addMark);
+button4.addEventListener('click', addMark);
+button5.addEventListener('click', addMark);
+button6.addEventListener('click', addMark);
+button7.addEventListener('click', addMark);
+button8.addEventListener('click', addMark);
+button9.addEventListener('click', addMark);
+button10.addEventListener('click', addMark);
