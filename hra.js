bb925267d0a6d9e74b0db2f754ad96aa447f12c2
Daniela Schneiderová <kozlovadaniela8@gmail.com>
2023-04-19 19:26:01 +0200

    Doplnění Bonus-remíza a replay-button/Additional bonus - replay and draw

--- hra.js
+++ hra.js
@@ -28,6 +28,8 @@ const addMark = (event) => {
     console.log(currentPlayer);
     currentPlayer = 'circle';
   }
+  //The addMark function is called when a button is clicked. It adds the symbol of the current player (circle or cross) to the button and disables it. It also updates the player's symbol in the dot element.
+
   const dotElm = document.querySelector('span.dot');
   dotElm.style.borderColor =
     currentPlayer === 'cross' ? 'transparent' : 'rgb(255, 255, 255)';
@@ -38,9 +40,8 @@ const addMark = (event) => {
         return 'o';
       } else if (button.classList.contains('fieldCross')) {
         return 'x';
-      } else {
-        return '_';
       }
+      return '_';
     },
   );
 
@@ -50,6 +51,12 @@ const addMark = (event) => {
       alert(`Vyhrál hráč se symbolem ${winner}.`);
       location.reload();
     }, 300);
+  } else if (winner === 'tie') {
+    /*Pokud hra skončila remízou, zobraz o tom uživateli zprávu.*/
+    setTimeout(() => {
+      alert('Hra skončila remízou!');
+      location.reload();
+    }, 250);
   }
 };
 
@@ -57,3 +64,12 @@ const buttonElm = document.querySelectorAll('button');
 buttonElm.forEach((buttonElm) => {
   buttonElm.addEventListener('click', addMark);
 });
+//buttonElm variable selects all the buttons on the game board, and adds a click event listener to each one that calls the addMark function.
+
+document.querySelector('.replay').addEventListener('click', (event) => {
+  const userAnswer = confirm('Opravdu chceš začít znovu?');
+  if (!userAnswer) {
+    event.preventDefault();
+  }
+});
+/*this code add click-event-listener to element with class "replay". If element is clicked, display with confirm dialog pop up. It asking user, if they want start over. For disabling the function (reloading page) is there event.preventDefault.*/
