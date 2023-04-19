2aa89a8d774a3471f6c4ff630d18c74754f19a0f
Daniela Schneiderová <kozlovadaniela8@gmail.com>
2023-04-19 16:37:26 +0200

    změna querySelectorAll, posunutí tabulky hry. Change the function querySelector to querySelectorAll. Move the body element up

--- hra.js
+++ hra.js
@@ -1,3 +1,4 @@
+import { findWinner } from 'https://unpkg.com/piskvorky@0.1.4';
 console.log('funguju');
 
 let currentPlayer = 'circle';
@@ -8,17 +9,6 @@ const playerCross = `
     <line x1="0" y1="20" x2="20" y2="0" stroke="white" stroke-width="2"/>
   </svg>
 `;
-
-const buttonElm = document.querySelector('button');
-const button2 = document.querySelector('button:nth-child(2)');
-const button3 = document.querySelector('button:nth-child(3)');
-const button4 = document.querySelector('button:nth-child(4)');
-const button5 = document.querySelector('button:nth-child(5)');
-const button6 = document.querySelector('button:nth-child(6)');
-const button7 = document.querySelector('button:nth-child(7)');
-const button8 = document.querySelector('button:nth-child(8)');
-const button9 = document.querySelector('button:nth-child(9)');
-const button10 = document.querySelector('button:nth-child(10)');
 
 const addMark = (event) => {
   if (currentPlayer === 'circle') {
@@ -42,14 +32,7 @@ const addMark = (event) => {
     currentPlayer === 'cross' ? 'transparent' : 'rgb(255, 255, 255)';
   console.log(dotElm, 'toto je dotelm');
 };
-
-buttonElm.addEventListener('click', addMark);
-button2.addEventListener('click', addMark);
-button3.addEventListener('click', addMark);
-button4.addEventListener('click', addMark);
-button5.addEventListener('click', addMark);
-button6.addEventListener('click', addMark);
-button7.addEventListener('click', addMark);
-button8.addEventListener('click', addMark);
-button9.addEventListener('click', addMark);
-button10.addEventListener('click', addMark);
+const buttonElm = document.querySelectorAll('button');
+buttonElm.forEach((buttonElm) => {
+  buttonElm.addEventListener('click', addMark);
+});
